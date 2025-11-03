4574a8576d576a0a400857ee88d1e6ae4e6e6a56
suntao82878380 <82878380@qq.com>
2025-11-03 10:20:37 +0800

    feat

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/vue2-gh-pages/'  // 仓库名必须与实际一致
+    : '/'
+}
